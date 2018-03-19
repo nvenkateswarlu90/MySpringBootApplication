6e0ee43462b129cc104b0d91067a752be375520a
Venkat <Venkat@192.168.1.48>
2018-03-19 19:17:22 +0530

    initial commit

---
+++ MySpringBootApplication/src/main/java/com/a4tech/MyApplicationController.java
@@ -0,0 +1,22 @@
+package com.a4tech;
+
+import java.lang.reflect.Method;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyApplicationController {
+	
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+	public String home(){
+		return "index";
+	}
+	@RequestMapping(value ="/welcome",method= RequestMethod.GET)
+	public String welcome(){
+		return "welcomePage";
+	}
+
+}
---
+++ MySpringBootApplication/src/test/java/com/a4tech/MySpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.a4tech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
